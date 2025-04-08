d94f9de021901f6ff917828d887b690eca3cbdca
Tomas Raffo Magnasco <tomasraffom@gmail.com>
2025-04-08 00:04:05 -0300

    Resta

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 def suma (a, b):
     return a + b
 
+def resta(a, b):
+    return a + b    
+
 print(suma(1,2))
--- test_main.py
+++ test_main.py
@@ -8,5 +8,12 @@ class TestSuma(unittest.TestCase):
     def test_suma_negativa(self):
         self.assertEqual(suma(-1, -1), -2)
 
+class TestResta(unittest.TestCase):
+    def test_resta_positiva(self):
+        self.assertEqual(resta(3, 1), 2)
+
+    def test_resta_negativa(self):
+        self.assertEqual(resta(-2, -2), 0)
+
 if __name__ == '__main__':
     unittest.main()
